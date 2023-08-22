07268e3c663e50fd342a192812a021fc84d1bd2b
Navruzbek2103 <navrozbek2306@gmail.com>
2023-08-23 01:50:26 +0500

    Added About page

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { Link, NavLink } from "react-router-dom";
 import "./style.scss";
 import SiteLogo from "./../../assets/images/site-logo.webp"
-
-
+import { AiOutlineInstagram } from "react-icons/ai";
+import { BiLogoFacebookCircle } from "react-icons/bi";
+import { AiFillYoutube } from "react-icons/ai"
 
 const index = () => {
+   let a = "lorem"
+   let date = new Date();
+   let result = date.getFullYear()
+   console.log(result)
    return (
       <footer className='footer'>
          <div className="container">
@@ -15,22 +20,44 @@ const index = () => {
                </Link>
                <ul className="footer__menu-list">
                   <li className="footer__menu-item">
-                     <NavLink to="" className="footer__menu-item-link">
+                     <NavLink to="/branches" className="footer__menu-item-link">
                         Filiallar
                      </NavLink>
                   </li>
                   <li className="footer__menu-item">
-                     <NavLink to="" className="footer__menu-item-link">
+                     <NavLink to="/about" className="footer__menu-item-link">
                         Biz haqimizda
                      </NavLink>
                   </li>
                   <li className="footer__menu-item">
-                     <NavLink to="" className="footer__menu-item-link">
+                     <NavLink to="/contacts" className="footer__menu-item-link">
                         Bog'lanish
                      </NavLink>
                   </li>
                </ul>
             </div>
+
+            <address className='footer__address'>
+               <p className="footer__address-copyright">© Delever 2020 - {result} Barcha huquqlar himoyalangan</p>
+
+               <ul className="footer__address-socialList">
+                  <li className="footer__address-socialItem">
+                     <a href="https://www.instagram.com/maxwayuz/" target='_blank' className="footer__address-socialItem-link">
+                        <AiOutlineInstagram className='footer__address-socialItem-link-icon' />
+                     </a>
+                  </li>
+                  <li className="footer__address-socialItem">
+                     <a href="https://www.facebook.com/maxway.uzb/" target='_blank' className="footer__address-socialItem-link">
+                        <BiLogoFacebookCircle className='footer__address-socialItem-link-icon' />
+                     </a>
+                  </li>
+                  <li className="footer__address-socialItem">
+                     <a href="https://www.youtube.com/@maxway2010" target='_blank' className="footer__address-socialItem-link">
+                        <AiFillYoutube className='footer__address-socialItem-link-icon' />
+                     </a>
+                  </li>
+               </ul>
+            </address>
          </div>
       </footer>
    );
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import "./style.scss";
+import HeroBannerImage from "./../../assets/images/about-page-banner.webp";
+
+
 const index = () => {
    return (
-      <div>
-         <h1>About page</h1>
+      <div className='about-page'>
+         <div className="container about-page__wrapper">
+            <h2 className="about-page__title">Kompaniya haqida</h2>
+            <div className="about-page__imgBox">
+               <img src={HeroBannerImage} alt="this image is the company image" className="about-page__imgBox-img" />
+            </div>
+            <p className="about-page__desc">
+               Kompaniya 2005 yilning fevral oyida Toshkent shahrida tashkil etilgan. Hozirda kompaniyaning Toshkent shahrida 18 ta filiali mavjud. <br />
+               Taomnoma asosan klаb sendvichlari, hot-doglar, burgerlar, lavashlar va donarlardan iborat. Bizning ustuvor yo'nalishlarimiz - ingredientlarning yangiligi va sifati, qo'shimchalarning xilma-xilligi, arzon narxlar va mehmonlarning takliflariga e'tibor. <br />
+               Har kuni turli xil odamlar MaxWay-dan buyurtma berishadi. Biz esa tashrif buyuruvchilar sonini ham, filiallar sonini ham oshirishga harakat qilmoqdamiz. Har bir tayyorlangan taom bilan biz o'ziga xos retseptlarning tafsilotlarini aniqlaymiz va sizning sevimli brendingiz bo'lishda davom etish uchun narx va sifatning mukammal muvozanatini qidiramiz. <br />
+               Agar siz kutilmaganda biz tomondan yomon xizmat yoki past sifatli pishirilgan taomga duch kelsangiz, bizga @maxwaymasterfood_bot manziliga murojaat qiling. Biz ijobiy va salbiy fikr-mulohazalarni mamnuniyat bilan qabul qilamiz. Mehmonning shikoyati - bu sovg'a, bu tufayli bizda o'sish uchun maqsad bo’ladi.
+            </p>
+         </div>
       </div>
    );
 };
