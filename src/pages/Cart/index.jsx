01c8a31a28cfe1d2ad503fff793c9c4334147007
Navruzbek2103 <navrozbek2306@gmail.com>
2023-09-04 11:52:21 +0500

    Added Cart page and Contact page

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import "./style.scss";
+import { Link } from 'react-router-dom';
+import emptyCartImg from "./../../assets/images/empty-cart.svg"
+
 
 const index = () => {
    return (
-      <div>
-         <h1>Cart page</h1>
+      <div className='cart-page'>
+         <div className="container">
+            <div className="cart-page__box">
+               <img src={emptyCartImg} alt="Shopping cart" className="cart-page__box-img" />
+               <h2 className="cart-page__box-title">Sizning savatingiz hozircha bo'sh</h2>
+               <p className="cart-page__box-text">Buyurtma qilingan narsalar shu yerda paydo bo'ladi.</p>
+               <Link to="/" className='cart-page__box-link'>Menyuga qaytish</Link>
+            </div>
+         </div>
       </div>
    );
 };
--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import "./style.scss";
+import { BiLogoTelegram } from "react-icons/bi"
+import ImgQRCode from "./../../assets/images/QR-code.jpg"
 
 const index = () => {
    return (
-      <div>
-         <h1>Contacts</h1>
+      <div className='contact-page'>
+         <div className="container">
+            <address className="contact-page__box">
+               <h2 className="contact-page__box-title">Bog'lanish</h2>
+               <div className="contact-page__box-inner">
+                  <div className="contact-page__box-inner-imgBox">
+                     <img src={ImgQRCode} alt="QR-code image" className="contact-page__box-inner-imgBox-img" />
+                  </div>
+                  <p className="contact-page__box-inner-desc">Telegramda sharh qoldiring yoki savol bering</p>
+                  <a href="https://t.me/maxwaymasterfood_bot" className="contact-page__box-inner-link"><BiLogoTelegram className='contact-page__box-inner-link-icon'/> @maxwaymasterfood_bot</a>
+               </div>
+               <h3 className="contact-page__box-subTitle">Yagona aloqa markazi</h3>
+               <a href='tel:+998712005400' className="contact-page__box-desc">+998712005400</a>
+            </address>
+         </div>
       </div>
    );
 };
